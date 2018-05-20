2988de29a1ebf9e3b9cd37cd04a04157b0d52917
Shan Desai <des@biba.uni-bremen.de>
2018-05-20 13:37:29 +0200

    Added Logging and Exception Handling
    - set `ERROR` Level logging for scripts
    - set log file destination to `/var/log/`
    
    Signed-off-by: Shan Desai <des@biba.uni-bremen.de>

--- gpsinflux/db.py
+++ gpsinflux/db.py
@@ -1,17 +1,32 @@
 import json
 import os
 from influxdb import InfluxDBClient
+import logging
+
+logger = logging.getLogger("influxdb")
+logger.setLevel(logging.ERROR)
+
+handler = logging.FileHandler("/var/log/gps.log")
+handler.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s-%(name)s-%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def connect_db():
     """Configure a Database using the configuration file"""
-    conf_file_path = os.getcwd() + '/configuration.json'
+    conf_file_path = os.getcwd() + '/conf.json'
     with open(conf_file_path, 'r') as conf:
         conf_data = json.load(conf)
+    logger.info('DB from Conf. Loaded')
     db_conf_info = conf_data['db']
+    logger.debug(db_conf_info)
+
     try:
         db = InfluxDBClient(host=db_conf_info['host'],
                             port=db_conf_info['port'],
                             database=db_conf_info['name'])
     except Exception as e:
-        raise e
+        logger.exception(e)
+        pass
     return db
--- gpsinflux/main.py
+++ gpsinflux/main.py
@@ -1,18 +1,29 @@
 import json
 import os
+import logging
 from gpsinflux.gps import read_and_parse, write_serial
+
+logger = logging.getLogger("main")
+logger.setLevel(logging.ERROR)
+
+handler = logging.FileHandler("/var/log/gps.log")
+handler.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s-%(name)s-%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def main():
 
-    conf_file_path = os.getcwd() + '/configuration.json'
+    conf_file_path = os.getcwd() + '/conf.json'
     with open(conf_file_path, 'r') as conf:
         conf_data = json.load(conf)
+    logger.info('got gps conf')
     db_conf_info = conf_data['gps']
 
     serialport = db_conf_info['serialport']
     baudrate = db_conf_info['baudrate']
-    try:
-        write_serial(serialport)
-        read_and_parse(serialport, baudrate)
-    except Exception as e:
-        raise e
+    logger.info('setting RMC and GGA sentences')
+    write_serial(serialport)
+    logger.info('acquiring information')
+    read_and_parse(serialport, baudrate)
